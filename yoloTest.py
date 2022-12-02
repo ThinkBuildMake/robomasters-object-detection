d6e473e8c6d4776fd7c8741c483325c9a828dc4a
Michael Chuang <michael.chuang@utexas.edu>
2022-12-02 14:53:28 -0600

    Adds requirements, a gitignore, and fills in yolov5

--- yoloTest.py
+++ yoloTest.py
@@ -10,3 +10,14 @@ from tqdm import tqdm
 from PIL import Image, ImageDraw
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Read images and annotations
+images = [os.path.join('images', x) for x in os.listdir('images')]
+annotations = [os.path.join('annotations', x) for x in os.listdir('annotations') if x[-3:] == "txt"]
+
+images.sort()
+annotations.sort()
+
+# Split the dataset into train-valid-test splits 
+train_images, val_images, train_annotations, val_annotations = train_test_split(images, annotations, test_size = 0.2, random_state = 1)
+val_images, test_images, val_annotations, test_annotations = train_test_split(val_images, val_annotations, test_size = 0.5, random_state = 1)
