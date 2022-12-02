8e8e262d1da8617a315632c17f14ba335a24c0b6
Michael Chuang <michael.chuang@utexas.edu>
2022-12-02 16:15:45 -0600

    Adds tutorial code to repository

--- yoloTest.py
+++ yoloTest.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 
 # Read images and annotations
 images = [os.path.join('images', x) for x in os.listdir('images')]
-annotations = [os.path.join('annotations', x) for x in os.listdir('annotations') if x[-3:] == "txt"]
+annotations = [os.path.join('labels', x) for x in os.listdir('labels') if x[-3:] == "xml"]
 
 images.sort()
 annotations.sort()
@@ -21,3 +21,20 @@ annotations.sort()
 # Split the dataset into train-valid-test splits 
 train_images, val_images, train_annotations, val_annotations = train_test_split(images, annotations, test_size = 0.2, random_state = 1)
 val_images, test_images, val_annotations, test_annotations = train_test_split(val_images, val_annotations, test_size = 0.5, random_state = 1)
+
+#Utility function to move images 
+def move_files_to_folder(list_of_files, destination_folder):
+    for f in list_of_files:
+        try:
+            shutil.move(f, destination_folder)
+        except:
+            print(f)
+            assert False
+
+# Move the splits into their folders
+move_files_to_folder(train_images, 'images/train')
+move_files_to_folder(val_images, 'images/val/')
+move_files_to_folder(test_images, 'images/test/')
+move_files_to_folder(train_annotations, 'labels/train/')
+move_files_to_folder(val_annotations, 'labels/val/')
+move_files_to_folder(test_annotations, 'labels/test/')
